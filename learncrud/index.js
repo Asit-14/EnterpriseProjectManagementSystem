91070f2b7aaace87b82ff2e73485e5caa8536256
Asit-14 <asitshakya789@gmail.com>
2026-01-19 16:16:51 +0530

    refactor: reorganize imports and streamline server initialization

--- learncrud/index.js
+++ learncrud/index.js
@@ -1,26 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
+import  dotenv from "dotenv"
+import User from "./model/user.model.js"
+import connectDB from "./config/database.js";
+import userRouter from "./routes/user.route.js";
 
+
+dotenv.config();
 const app = express();
 const port = 3000;
 
-// MongoDB connection string
+app.use(express.json()); // converts JSON request body
+app.use("/", userRouter);
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(mongoURL);
-        console.log("Database connected successfully");
-    } catch (error) {
-        console.error("Database connection failed:", error);
-        process.exit(1); // stop server if DB fails
-    }
-};
 
-app.get("/", (req, res) => {
-    res.send("This is home page");
-});
 
-// Start server only after DB connection
+
+
+
+// Start server after DB connection
 connectDB().then(() => {
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
